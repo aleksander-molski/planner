c706f492cee51127fe3cdf898205685830d1ff35
Aleksander Molski <aleksandermol@interia.pl>
2021-07-16 07:53:00 +0200

    create to-do-task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoTaskComponent } from './to-do-task/to-do-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/to-do-task/to-do-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-to-do-task',
+    templateUrl: './to-do-task.component.html',
+    styleUrls: ['./to-do-task.component.scss']
+})
+export class ToDoTaskComponent {
+
+}
